484295efdf3ac8e6054f8a42ad100e70daca1385
Pharoouzy <pharoouzy@yahoo.com>
2018-12-03 18:34:48 +0100

    Update geocode.js

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -2,16 +2,17 @@ const request = require('request');
 
 var geocodeAddress = (address, callback) => {
     var encodedAddress = encodeURIComponent(address);
+    var apiKey = '**************************************';
     // console.log(encodedAddress);
     //1301%20lombard%20street%20philadelphia
     // The request() takes 2 arg, options obj where we can config info and
     // 2nd arg is a callback function, that get call once the data comes back from the HTTP endpoint
     request({
-        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=`,
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${apiKey}`,
         json: true
     }, (error, response, body) => {
         //console.log(body);// Logs the json file content on the screen
-        // second arg in stringify is user to filter properties, the 3rd arg is gonna format the JSON and u specify the amount of spaces u wanna use per indentation
+        // second arg in stringify is used to filter properties, the 3rd arg is gonna format the JSON and u specify the amount of spaces u wanna use per indentation
         // console.log(JSON.stringify(body, undefined, 2));// takes the json file contents, convert it to JS obj and Logs o the screen
         // console.log(JSON.stringify(response, undefined, 2));// The response obj prints a complex obj, it has header, body, request, and own header which get set when json is set true
         // console.log(JSON.stringify(error, undefined, 2));// error obj contains error made when making HTTP request or error from the API
